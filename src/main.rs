370082ebc609a4bcec4c12021269d29618b81b9d
simtf <fabiensimonet@gmail.com>
2024-12-03 21:40:27 +0100

    Day 1

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use std::env;
+
+mod day_1;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let day: &str = args.get(1).expect("You should specify the day to execute.");
+
+    match day {
+        "1" => day_1::execute(),
+        _ => println!("Day {day} is not implemented !"),
+    }
+}
